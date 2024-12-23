199f5746aea5465e902046e52238f4a46029b4e8
JosueGonzalezMontiel <143673540+JosueGonzalezMontiel@users.noreply.github.com>
2024-12-22 22:34:46 -0600

    8.0
    esta version si concta

--- conexion.py
+++ conexion.py
@@ -2,6 +2,6 @@ from peewee import *
 
 database= MySQLDatabase(
     'infoapi',
-    user='root', password='jgm981219Y',
+    user='prueba1', password='jgm981219Y',
     host='34.31.105.197', port=3306
 )
